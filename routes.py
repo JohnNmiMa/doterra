8c30e0aecc75ad0b55956bda7d2015ecf4b9abf4
John <johnmarksjr@gmail.com>
2014-06-26 15:50:11 -0600

    Refactored the edit_sample.html & publish_sample.html into sample.html
    Improved how to grab image on Windows

--- routes.py
+++ routes.py
@@ -24,14 +24,14 @@ def edit_sample(sample):
         # Remote "Wellness Advocate" from the form['welladv'] field
         form['welladv'] = form['welladv'].split('#')[1]
 
-    return render_template('edit_sample.html', page=path, sample=sample, formdata=form)
+    return render_template('sample.html', page=path, sample=sample, formdata=form)
 
 @app.route('/publish_sample/<path:sample>', methods = ['GET', 'POST'])
 def publish_sample(sample):
     #pdb.set_trace()
     path = request.path.split('/')[1]
     #print('path = {}').format(path)
-    return render_template('publish_sample.html',  page=path, sample=sample, formdata=request.form)
+    return render_template('sample.html',  page=path, sample=sample, formdata=request.form)
 
  
 if __name__ == '__main__':
--- static/js/application.js
+++ static/js/application.js
@@ -25,8 +25,12 @@ $(document).ready(function() {
                 '    <li>On Mac OS X, type "Shift+Command+4"' +
                 '    <li>On Windows:' +
                 '        <ol>' +
-                '        <li>select the browser window displaying the invitation' +
-                '        <li>type "Alt + PrtScn"' +
+                '        <li>Select the browser window displaying the invitation' +
+                '        <li>Press "Alt+Prtint Screen"' +
+                '        <li>In the Paint window, click Edit, and then click Paste' +
+                '        <li>Crop the image to contain only the invitation of interest' +
+                '        <li>When the image appears in the Paint window, click File, and then click Save As' +
+                '        <li>In the Save As dialog box, in the File name box, type a name for the screen shot, and then click Save' +
                 '        </ol>' +
                 '    </ul>' +
                 '<li>Edit the screenshot image if needed, then print or email to class attendees' +
